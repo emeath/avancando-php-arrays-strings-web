eb7a815ec04269aa624c8acd199963692d428a51
emeath <matheus.martins.jeronimo@gmail.com>
2022-04-03 20:09:19 -0300

    Percorrendo array em PHP

---
+++ 02-percorrendo-arrays.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$listaDeNumeros = [221, 432, 5122, 512];
+
+for($i = 0; $i < 4; $i++){
+    echo $listaDeNumeros[$i] . PHP_EOL;
+}
+
+echo "==========" . PHP_EOL;
+
+$listaGrandeDeNumeros = array(22, 21, 32, 453, 23, 123, 532, 12, 532, 6453, 675, 32, 4325, 643, 24);
+
+for( $i = 0; $i < count($listaGrandeDeNumeros); $i++ ) {
+    echo $listaGrandeDeNumeros[$i] . PHP_EOL;
+}
+
